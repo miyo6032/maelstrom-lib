12b342626199fa5cf86e87d46f3a4b9e4973d46f
Michael Yoshimura <miyo6032@colorado.edu>
2021-01-17 19:36:34 -0700

    Register test commands only in dev mode

--- src/main/kotlin/net/barribob/maelstrom/MaelstromMod.kt
+++ src/main/kotlin/net/barribob/maelstrom/MaelstromMod.kt
@@ -1,5 +1,6 @@
 package net.barribob.maelstrom
 
+import net.barribob.maelstrom.MaelstromMod.isDevelopmentEnvironment
 import net.barribob.maelstrom.general.command.TestArgumentType
 import net.barribob.maelstrom.general.command.TestCommand
 import net.barribob.maelstrom.general.event.EventScheduler
@@ -14,6 +15,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.command.argument.ArgumentTypes
 import net.minecraft.command.argument.serialize.ConstantArgumentSerializer
 import net.minecraft.util.Identifier
@@ -42,12 +44,26 @@ object MaelstromMod {
 
     val debugPoints = DebugPointsNetworkHandler()
     val testCommand = TestCommand(InGameTests(debugPoints))
+
+    val isDevelopmentEnvironment = FabricLoader.getInstance().isDevelopmentEnvironment
 }
 
 @Suppress("unused")
 fun init() {
     ServerTickEvents.START_SERVER_TICK.register(ServerTickEvents.StartTick { MaelstromMod.serverEventScheduler.updateEvents() })
 
+    if(isDevelopmentEnvironment) initDev()
+}
+
+@Environment(EnvType.CLIENT)
+@Suppress("unused")
+fun clientInit() {
+    ClientTickEvents.START_CLIENT_TICK.register(ClientTickEvents.StartTick { MaelstromMod.clientEventScheduler.updateEvents() })
+
+    if(isDevelopmentEnvironment) clientInitDev()
+}
+
+private fun initDev() {
     CommandRegistrationCallback.EVENT.register(MaelstromMod.testCommand)
     ArgumentTypes.register(
         "${MaelstromMod.MODID}:libtest",
@@ -56,9 +72,7 @@ fun init() {
 }
 
 @Environment(EnvType.CLIENT)
-@Suppress("unused")
-fun clientInit() {
-    ClientTickEvents.START_CLIENT_TICK.register(ClientTickEvents.StartTick { MaelstromMod.clientEventScheduler.updateEvents() })
+private fun clientInitDev() {
     ClientPlayNetworking.registerGlobalReceiver(MaelstromMod.DRAW_POINTS_PACKET_ID) { client, _, buf, _ ->
         MaelstromMod.debugPoints.drawDebugPointsClient(client, buf)
     }
